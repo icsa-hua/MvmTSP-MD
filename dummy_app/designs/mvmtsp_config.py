fix: normalization & central hubs(depots)

dummy_app/designs/mvmtsp_config.py
@@ -18,7 +18,7 @@ import random
 import networkx as nx
 from pathlib import Path
 from sklearn.preprocessing import MinMaxScaler 
-
+from collections import defaultdict
 import pdb
 from k_means_constrained import KMeansConstrained
 
@@ -36,17 +36,15 @@ class MVMTSPConfig(ABC):
         self.distance_columns:List[str] = []
         self.energy_columns:List[str] = []
         self.travel_time_columns:List[str] = []
-        self.normalized_battery:np.ndarray = np.ndarray((0,0))
-        self.max_battery_norm:float = 0.0
+        self.move_energy:np.ndarray = np.ndarray((0,0))
         self.average_energy:float = 0.0
         self.depots:Optional[np.ndarray] = None
         self.distance_metric:str = "euclidean"
         self.average_coverage_energy:float=0.0
-        self.normalized_coverage_energy:float=0.0
         self.user_points:Dict[int,Tuple[float,float]] = {}  # User points for regionalization
         self.scenario:str = ""
-        self.ascend_avg_energy:float = 0.0
-        self.descend_avg_energy:float = 0.0 
+        self.ascend_energy = pd.DataFrame
+        self.descend_energy= pd.DataFrame
       
 
     @abstractmethod
@@ -103,7 +101,19 @@ class MVMTSPConfig(ABC):
 
     
     @abstractmethod 
-    def preprocess(self, distances_path:Union[str,Path], energies_path:Union[str,Path], nodes_path:Union[Path, str], agents:int, customers_path:Union[Path,str], ground_users:Any, max_battery:int)->pd.DataFrame:
+    def preprocess(
+        self,
+        distances_path:Union[str,Path],
+        energies_path:Union[str,Path], 
+        nodes_path:Union[Path, str], 
+        num_of_agents:int, 
+        v_ver:float, 
+        v_hor:float, 
+        customers_path:Union[Path,str], 
+        user_points:Any, 
+        max_battery:int, 
+        altitude:int
+    )->pd.DataFrame:
 
         def normalize_data(df:pd.DataFrame)->pd.DataFrame:
             
@@ -125,17 +135,8 @@ class MVMTSPConfig(ABC):
         self.V = nodes 
         self.v = len(self.V)
         
-        self.agents = list(range(1,agents+1))
+        self.agents = list(range(1,num_of_agents+1))
         self.max_battery = max_battery
-        max_battery_norm = np.ones(len(energies.columns)) * max_battery 
-
-        temporary_dataframe_energy = energies.copy() 
-        temporary_dataframe_energy.loc[len(energies)] = max_battery_norm
-        temporary_dataframe_energy = normalize_data(temporary_dataframe_energy)
-        max_battery_norm = temporary_dataframe_energy.iloc[-1]
-        temporary_dataframe_energy.drop([len(energies)], inplace=True)
-        self.normalized_battery = temporary_dataframe_energy.values
-        self.max_battery_norm = max_battery_norm.iloc[0]
 
         energy_model = DroneEnergyModel()
         
@@ -144,9 +145,6 @@ class MVMTSPConfig(ABC):
         self.normalized_coverage_energy = self.average_coverage_energy / max_battery  # Normalize coverage energy
         logger.debug(f"Average coverage energy: {self.average_coverage_energy} Wh")
 
-        deallocate_memory(temporary_dataframe_energy)
-        deallocate_memory(max_battery)
-        deallocate_memory(max_battery_norm)
 
         # Prepare Matrices 
         dist_columns = [f'dist_{i}' for i in range(1, self.v + 1)]
@@ -170,9 +168,6 @@ class MVMTSPConfig(ABC):
         distances = normalize_data(distances)
         energies = normalize_data(energies)
         travel_times = normalize_data(travel_times)
-
-        # Format customers 
-        self.user_points = ground_users if ground_users else {}
 
         # Setup visits allowed 
         # self.allowed_visits = np.full(self.v, len(self.agents), dtype=int)
@@ -217,11 +212,15 @@ class MVMTSPConfig(ABC):
             
             return pd.DataFrame(scaled_data, columns=df.columns, index=df.index)
 
+        data_path = f"{os.getcwd()}/assets/data"
+        if not os.path.exists(data_path):
+            os.mkdir(data_path)
 
         self.max_battery = max_battery
 
         distances = pd.DataFrame(distance_matrix, columns=[f'dist_{i}' for i in range(1, len(distance_matrix)+1)])
-        
+        distances.to_csv(f"{data_path}/distances.csv")
+
         energy_model = DroneEnergyModel(
             v_hor=v_hor, 
             v_ver=v_ver,
@@ -239,24 +238,13 @@ class MVMTSPConfig(ABC):
 
         # NOTE: To convert it to Wh 
         energies = energies / 3600.0
-
-        max_battery_norm = np.ones(len(energies.columns)) * max_battery 
-        temporary_dataframe_energy = energies.copy() 
-        temporary_dataframe_energy.loc[len(energies)] = max_battery_norm
-        temporary_dataframe_energy = normalize_data(temporary_dataframe_energy)
-        max_battery_norm = temporary_dataframe_energy.iloc[-1]
-        temporary_dataframe_energy.drop([len(energies)], inplace=True)
-        self.normalized_battery = temporary_dataframe_energy.values
-        self.max_battery_norm = max_battery_norm.iloc[0]
-
-        self.average_coverage_energy = energy_model.coverage_energy(1250) # In J 
+        energies.to_csv(f"{data_path}/energies.csv")
+        
+        self.move_energy = energies.values.astype(np.float32)
+        self.average_coverage_energy = energy_model.coverage_energy(altitude) # In J 
         self.average_coverage_energy = self.average_coverage_energy / 3600.0  # Convert to Wh
-        self.normalized_coverage_energy = self.average_coverage_energy / max_battery  # Normalize coverage energy
-        logger.debug(f"Average coverage energy: {self.average_coverage_energy} Wh")
 
-        deallocate_memory(temporary_dataframe_energy)
-        deallocate_memory(max_battery)
-        deallocate_memory(max_battery_norm)
+        logger.debug(f"Average coverage energy: {self.average_coverage_energy} Wh")
 
         area_ids = list(user_points.keys()) 
         centroids_x = [centroids[i][0] for i in range(len(centroids))]
@@ -274,39 +262,38 @@ class MVMTSPConfig(ABC):
         self.average_energy = float(np.average(energies))
 
         # Calculate Average Ascend and Descend 
-        ascend = [] 
-        descend = [] 
-
+        ascend_energy = defaultdict(list)
+        descend_energy = defaultdict(list)
         for depot in depots: 
             for i in self.V['Area_id']: 
-                if i in depots: continue 
-                ascend.append(energy_model.ascend_energy(
+                
+                ascend_energy[(int(depot))].append(np.float32((energy_model.ascend_energy(
                     current_node=depot, 
                     next_node=i,
                     altitude=altitude,
                     distance_matrix=distance_matrix
-                ))
+                )) / 3600))
 
-                descend.append(energy_model.descend_energy(
+                descend_energy[(int(depot))].append(np.float32((energy_model.descend_energy(
                     current_node=i, 
                     next_node=depot, 
                     altitude=altitude, 
                     distance_matrix=distance_matrix
-                ))
-
-        self.ascend_avg_energy = float(np.average(ascend)) / 3600 # From J to Wh
-        self.descend_avg_energy = float(np.average(descend)) / 3600 # From J to Wh
+                )) / 3600))
+        
+        self.ascend_energy = pd.DataFrame.from_dict(ascend_energy).T
+        self.descend_energy = pd.DataFrame.from_dict(descend_energy).T
 
         # Here distance must be in meters
         travel_times = (distance_matrix * 1e3) / v_hor / 60.0  # Convert to minutes
         travel_times = pd.DataFrame(travel_times, columns=[f'tt_{i}' for i in range(1, len(travel_times)+1)])
         self.travel_cost = travel_times.values
-
+        travel_times.to_csv(f'{data_path}/times.csv')
         assert distances.shape == energies.shape == travel_times.shape, "Distances, energies, and travel times must have the same shape"
 
-        distances = normalize_data(distances)
-        energies = normalize_data(energies)
-        travel_times = normalize_data(travel_times)
+        distances = normalize_data(distances,name='distance_km')
+        energies = normalize_data(energies, name='energies_Wh')
+        travel_times = normalize_data(travel_times, name='times_min')
 
         self.distance_columns = distances.columns.tolist()
         self.energy_columns = energies.columns.tolist()
@@ -373,14 +360,12 @@ class MVMTSPConfig(ABC):
 
         # TODO: Calculate Maximum nodes based on Hover.
         max_nodes = 0 
-        # if self.scenario == "coverage": 
-        #     adjusted_energy = self.average_energy + self.average_coverage_energy 
-        #     max_nodes = int(self.max_battery / adjusted_energy)
-        # elif self.scenario == "energy": 
-        #     max_nodes = int(self.max_battery / self.average_energy)
         
+        # Reserve 10–15% for emergency return
+        reserve = self.max_battery * 0.15
         adjusted_energy = self.average_energy + self.average_coverage_energy 
-        max_nodes = int(self.max_battery / adjusted_energy) - 1
+        
+        max_nodes = int((self.max_battery-reserve) / adjusted_energy) - 1
 
         logger.debug(f"Maximum nodes per cluster based on battery: {max_nodes}")
         charge_points = int(np.floor(self.v/max_nodes))
@@ -429,12 +414,12 @@ class MVMTSPConfig(ABC):
 
 
     @abstractmethod
-    def cluster_prioritization(self, clusters:pd.core.groupby.generic.DataFrameGroupBy, cue_groups:Mapping[int,Any])->pd.DataFrame:
+    def cluster_prioritization(self, clusters:pd.core.groupby.generic.DataFrameGroupBy, cue_groups:Mapping[int,Any], distance_matrix)->pd.DataFrame:
         topsis = TOPSISPriority()
         cluster_criteria = {} 
          
         for cluster_id, cluster_df in clusters:
-            cluster_criteria[cluster_id] = topsis.gather_criteria(cluster_df, cue_groups=cue_groups, distance_metric=self.distance_metric)
+            cluster_criteria[cluster_id] = topsis.gather_criteria(cluster_df, cue_groups=cue_groups, distance_matrix=distance_matrix)
 
         priority = topsis.run_model(cluster_criteria, [])
         logger.debug("Cluster Prioritization (TOPSIS) Complete...")

dummy_app/models/central_hubs.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import numpy as np 
+import pandas as pd 
 from typing import Dict, List
 from sklearn.preprocessing import MinMaxScaler
 
@@ -13,53 +14,76 @@ class CentralHub:
         self.distance_centroid:np.ndarray = np.array([])   
         self.number_allowed_visits:Dict[int,int] = {}
 
-    def calculate_betweeness(self, G:nx.DiGraph, cluster_nodes:List[int], nodes_dict, cost_dist)->None:
+    def calculate_betweeness(self, G:nx.DiGraph, cluster_nodes:List[int], nodes_dict, cost_dist, depot_id)->None:
         if not nx.is_strongly_connected(G):
             print("Directed graph is not strongly connected.")
             return
-        
+
+        central_candidates = [n for n in G.nodes if n != depot_id]
+        reverse_dict = {v:k for k,v in nodes_dict.items()}
         if len(cluster_nodes) < 25: 
             centrality = nx.closeness_centrality(G, distance='cost')
+            centrality = {k: v for k, v in centrality.items() if k in central_candidates}
+
         else: 
             # TODO: Change this one to walk centrality for larger graphs. 
             centrality = nx.betweenness_centrality(G, weight='cost')
-        
-        self.betweeness = np.array([centrality[nodes_dict[n]] for n in cluster_nodes]).reshape(-1, 1)
+
+        self.betweeness = np.array([centrality[nodes_dict[n]] for n in cluster_nodes if n != reverse_dict[depot_id]]).reshape(-1, 1)
         
 
-    def get_node_with_min_total_distance(self, cluster_nodes, dist_matrix, nodes_dict):
-        total_dists = {i: sum(dist_matrix[nodes_dict[i]][nodes_dict[j]-1] for j in cluster_nodes if i != j) for i in cluster_nodes}
-        self.distance_centroid = np.array([total_dists[i] for i in cluster_nodes]).reshape(-1, 1)
+    def get_node_with_min_total_distance(self, cluster_nodes, dist_matrix, nodes_dict, depot_id):
+        reverse_dict = {v:k for k,v in nodes_dict.items()}
+
+        total_dists = {i: sum(dist_matrix[nodes_dict[i]][nodes_dict[j]-1] for j in cluster_nodes if i != j) for i in cluster_nodes if i != reverse_dict[depot_id]}
+        self.distance_centroid = np.array([total_dists[i] for i in cluster_nodes if i != reverse_dict[depot_id]]).reshape(-1, 1)
 
 
     def normalize_values(self):
-        scaler = MinMaxScaler() 
+        scaler = MinMaxScaler(feature_range=(0,1)) 
+        
         self.betweeness = scaler.fit_transform(self.betweeness).flatten()
         self.distance_centroid = scaler.fit_transform(self.distance_centroid).flatten()
     
 
-    def aggregate_scores(self, cluster_nodes): 
-        composite_score = {cluster_nodes[i]: (self.betweeness[i] + (1 - self.distance_centroid[i])) / 2 for i in range(len(cluster_nodes))}
+    def aggregate_scores(self, cluster_nodes,alpha=0.5): 
+        composite_score = {
+            cluster_nodes[i]: alpha * self.betweeness[i] + (1 - alpha) * (1 - self.distance_centroid[i])
+            for i in range(len(cluster_nodes) - 1)
+        }        
         return composite_score
     
 
-    def allowed_visits(self, cluster_nodes, n_agents): 
+    def allowed_visits(self, cluster_nodes, n_agents, bridge_nodes, composite_score): 
         max_visits = n_agents 
-        self.number_allowed_visits = {k: int(1 + self.betweeness[k] * (max_visits - 1)) for k in range(len(cluster_nodes))}
+
+        self.number_allowed_visits = {
+            k: int(1 + composite_score[k] * (max_visits - 1))
+            for k in range(len(cluster_nodes) - 1)
+        }
+
+        composite_dict = {
+            'composite_score': composite_score.values() 
+        }
+        composite_df = pd.DataFrame.from_dict(composite_dict)
+        bridge_nodes = int(composite_df.idxmax().iloc[0])
+       
         # Ensure at least one node gets more than 1 visit
         if all(v == 1 for v in self.number_allowed_visits.values()):
-            max_index = int(np.argmax(self.betweeness))
-            self.number_allowed_visits[max_index] = 2 
+            self.number_allowed_visits[bridge_nodes] = n_agents + 1 
 
             
 
     def get_bridge_nodes(self, graph:nx.DiGraph, cluster_nodes:List[int], cost_dist:Dict[int,np.ndarray], nodes_dict:Dict[int,int], n_agents:int )-> List[int]: 
-        self.calculate_betweeness(graph, cluster_nodes, nodes_dict,cost_dist)
-        self.get_node_with_min_total_distance(cluster_nodes, cost_dist, nodes_dict)
+
+        depot_id = nodes_dict[len(nodes_dict)-1]
+        self.calculate_betweeness(graph, cluster_nodes, nodes_dict,cost_dist,depot_id=depot_id)
+        self.get_node_with_min_total_distance(cluster_nodes, cost_dist, nodes_dict,depot_id=depot_id)
         self.normalize_values() 
-        self.allowed_visits(cluster_nodes, n_agents)
         composite_score = self.aggregate_scores(cluster_nodes)
-        bridge_nodes = max(1, int(0.1 * len(composite_score)))
+        bridge_nodes = max(1, int(0.1 * len(composite_score))) # consider only the top 10% of nodes (by composite score) to act as bridge or central nodes.
+        
+        self.allowed_visits(cluster_nodes, n_agents, bridge_nodes, composite_score)
 
         return [node for node, _ in sorted(composite_score.items(), key=lambda item: item[1], reverse=True)[:bridge_nodes]]
     

dummy_app/models/simulation_builder.py
@@ -1,5 +1,5 @@
 from dummy_app.designs.mvmtsp_config import MVMTSPConfig 
-from dummy_app.tools.common import deallocate_memory, extract_per_agent_metrics, calculate_totals_from_paths
+from dummy_app.tools.common import deallocate_memory, extract_per_agent_metrics, calculate_totals_from_paths, load_generated_data, calculate_recharge_steps
 from dummy_app.tools.performance_metrics import Metrics
 from dummy_app.designs.cluster import Cluster
 from dummy_app.designs.agents import TSPAgent 
@@ -21,7 +21,6 @@ from collections import defaultdict
 from geopy.distance import geodesic
 
 
-
 class Builder(MVMTSPConfig):
 
     def __init__(self, config:Dict[str,Any], trials:int, scenario:str='energy'): 
@@ -37,6 +36,8 @@ class Builder(MVMTSPConfig):
         self.num_constraints = 0 
         self.variables_count = 0
         self.Time = 0 
+        self.problem_results = defaultdict()
+        self.coverage_time = 10 
 
 
     def call_genetic_algorithm(self, nodes_dict:Dict[int,int], cost:Dict[str,float], depot:int, verbose:bool=False, population_size:int=200, generations:int=100)->Tuple[List[int],Any]:
@@ -47,7 +48,7 @@ class Builder(MVMTSPConfig):
         return super().assign_agents_to_areas(plethos, depots)
     
 
-    def assign_depot_to_cluster(self, clusters:Any, depots_df:pd.DataFrame):
+    def assign_depot_to_cluster(self, clusters:Any, depots_df:pd.DataFrame, distance_matrix):
 
         def find_duplicate_values(mapping:Dict)->Dict: 
             reverse = {} 
@@ -59,26 +60,24 @@ class Builder(MVMTSPConfig):
 
         for cluster_id, cluster_df in clusters:
             # Calculate cluster centroid directly
-            centroid = cluster_df[['X_coords', 'Y_coords']].mean().values 
+            centroid = cluster_df['Area_id']
 
             # Find nearest depot 
             min_dist = float('inf')
             best_depot:int = 0 
-            dist = 0.0
             for _, depot_row in depots_df.iterrows(): 
-                depot_coords = depot_row[['X_coords', 'Y_coords']].values
 
-                if self.distance_metric == 'euclidean':
-                    dist = np.linalg.norm(centroid - depot_coords) 
-
-                elif self.distance_metric == 'geodesic':
-                    dist = geodesic(centroid, depot_coords).km
-
-                if dist < min_dist:
-                    min_dist = dist
-                    best_depot = depot_row['Area_id']
+                depot_id = int(depot_row['Area_id'])
+                for centre in centroid: 
+                    dist = distance_matrix[depot_id,int(centre)]
+                    
+                    if dist < min_dist:
+                        min_dist = dist
+                        best_depot = depot_id
 
             cluster_depot[cluster_id] = int(best_depot)
+
+
         dulpicates = find_duplicate_values(self.depots_for_agents) 
         return cluster_depot, dulpicates
     
@@ -106,7 +105,6 @@ class Builder(MVMTSPConfig):
         depot_row= depot_row.copy()
         depot_row  = depot_row[depot_row['Area_id']==depot_id]
         depot_row.loc[:, 'cluster'] = np.mean(cluster_df[1]['cluster'])        
-        # Append and reset index if needed
         updated_cluster = pd.concat([cluster_df[1], depot_row], ignore_index=True)
         
         return updated_cluster
@@ -199,6 +197,7 @@ class Builder(MVMTSPConfig):
         for agent_name, agent_id in list_of_agents.items():
             edges = set()
             step = -1 
+            actual_time = 0
 
             current_node = reverse_dict[cluster.depot_id]
             iteration = 0 
@@ -236,19 +235,21 @@ class Builder(MVMTSPConfig):
                                 cluster.nodes_dict[next_node],
                                 timestep,
                             )
-
                             if triplet in edges:
                                 logger.debug(f"Edge {triplet[0], triplet[1]} already seen for agent {agent_name}")
                                 continue
 
                             # Optional: append once with duration, or multiple times
                             for d in range(duration):
-                                paths[agent_name].append((triplet[0], triplet[1], timestep + d))
+                                actual_time += 1
+                                paths[agent_name].append((triplet[0], triplet[1], actual_time))
 
-                            wait_step = timestep + duration 
+                            wait_step = timestep + duration + self.coverage_time
                             if wait_step in cluster.timeframe and cluster.wait[agent_id, wait_step].varValue == 1:
-                                wait_triplet = (cluster.nodes_dict[next_node], cluster.nodes_dict[next_node], wait_step)
-                                paths[agent_name].append(wait_triplet)
+                                for ii in range(1,self.coverage_time+1,1):
+                                    actual_time += 1
+                                    wait_triplet = (cluster.nodes_dict[next_node], cluster.nodes_dict[next_node], actual_time )
+                                    paths[agent_name].append(wait_triplet)
 
                             edges.add(triplet)
                             current_node = reverse_dict[triplet[1]]
@@ -291,11 +292,13 @@ class Builder(MVMTSPConfig):
         logger.debug("✅ Solutions validated successfully...")
         return paths 
 
+
     def createGeoDataset(self, data):
         return super().createGeoDataset(data)
      
 
     def run_model(self, distance_matrix:np.ndarray, data:pd.DataFrame, cue_groups:Dict[int,List[Any]])->Dict:
+        self.user_points = cue_groups
         logger.debug("Running combinatorial problem constructor...")
         with tqdm (total=8, desc="Preparing Problem with clustering") as pbar: 
 
@@ -318,7 +321,7 @@ class Builder(MVMTSPConfig):
 
             # Phase 2: Clustering and Prioritization 
             try: 
-                priority = self.cluster_prioritization(clusters, cue_groups)
+                priority = self.cluster_prioritization(clusters, cue_groups, distance_matrix)
                 pbar.update(1)
                 logger.debug("✅ Clusters prioritized successfully...")
             except Exception as e:
@@ -327,12 +330,11 @@ class Builder(MVMTSPConfig):
 
             # Phase 3: Agent Assignment for all clusters 
             try: 
-                cluster_with_depots, same_depot_agents = self.assign_depot_to_cluster(clusters, depots)
+                cluster_with_depots, same_depot_agents = self.assign_depot_to_cluster(clusters, depots, distance_matrix=distance_matrix)
                 pbar.update(1)
                 logger.debug("✅ Depots assigned to clusters successfully...")
                 assignments = self.allocate_agents_to_clusters(cluster_with_depots, priority, same_depot_agents)
                 pbar.update(1)
-
 
             except Exception as e:
                 logger.exception(f"❌ Error occurred during agent assignment: {e}")
@@ -421,8 +423,8 @@ class Builder(MVMTSPConfig):
         return super().regionalization(GDF)
     
     
-    def cluster_prioritization(self, clusters, cue_groups: Mapping[int, Any]):
-        return super().cluster_prioritization(clusters, cue_groups)
+    def cluster_prioritization(self, clusters, cue_groups: Mapping[int, Any], distance_matrix):
+        return super().cluster_prioritization(clusters, cue_groups, distance_matrix)
     
 
     def clustering(self, cluster, cluster_id, assignment, depot_id)->Dict:
@@ -444,6 +446,7 @@ class Builder(MVMTSPConfig):
         )
   
         logger.debug(f"Clustering with {cluster_id} and agents assigned to it: {assignment}")
+        
         # Step 2: Process inpute context 
         try: 
             cluster_object.prepare_context(
@@ -461,7 +464,9 @@ class Builder(MVMTSPConfig):
         if self.scenario == 'coverage':
             self.get_cluster_coverage(cluster_object)
 
-        cluster_object.R_points = list(np.ones(len(cluster_object.nodes_dict)))
+        # deallocate_memory(context)
+
+        # cluster_object.R_points = list(np.ones(len(cluster_object.nodes_dict)))
         # Step 4: Create and configure the optimization problem 
         try: 
             paths = cluster_object.problem_formulation(builder=self, scenario=self.scenario) 
@@ -473,12 +478,31 @@ class Builder(MVMTSPConfig):
         # paths = cluster_object.get_solution()
         # Step 6: Add the recharge phase & synchronize agents 
         
-        extract_per_agent_metrics(
+        cost = load_generated_data()
+        results = extract_per_agent_metrics(
             paths=paths, 
-            distance_costs=
+            distance_costs=cost['distances'], 
+            energy_costs=cost['energies'],
+            time_costs=cost['times'],
+            coverage_energy=self.average_coverage_energy
         )
+        totalDistance, totalEnergy, totalTime = calculate_totals_from_paths(
+            results=results
+        )
+
+        self.problem_results[f'Cluster_{cluster_object.id}'] = {
+            "scenario_name":self.scenario,
+            "agent_results":results, 
+            "Total Distance": totalDistance, 
+            "Total Energy":totalEnergy, 
+            "Total Time": totalTime, 
+            "Average Throughput": cluster_object.R, 
+            "Average SINR" : cluster_object.sinr
+        }
+
+        paths = self.synchronize_agent_paths(paths, cluster_object)
+        print("Amount of CONSTRAINTs: ", len(cluster_object.problem.constraints))
         pdb.set_trace()
-        paths = self.synchronize_agent_paths(paths, cluster_object, depot_id)
         deallocate_memory(cluster_object)
         return paths 
 
@@ -500,10 +524,7 @@ class Builder(MVMTSPConfig):
             "const_24":constraint_24, "const_25":constraint_25,
             "const_26":constraint_26, "const_27":constraint_27,
             "const_28":constraint_28, "const_29":constraint_29,
-            "const_30":constraint_30, "const_31":constraint_31,
-            "const_32":constraint_32, "const_33":constraint_33,
-            "const_34":constraint_34, "const_35":constraint_35
-        }
+            "const_30":constraint_30}
 
         
         employed_agents = ["Agent_" + str(agent_id) for agent_id in cluster.employed_agents]
@@ -520,8 +541,8 @@ class Builder(MVMTSPConfig):
                       cluster.x[node, next_node, header].setInitialValue(1) 
         
         logger.info(f"Running Through Constraints |")
-
-        for const in tqdm(available_constraints):
+        
+        for const in available_constraints:
             if const not in self.constraints: continue
             try: 
                 available_constraints[const](
@@ -530,7 +551,7 @@ class Builder(MVMTSPConfig):
                     V_nodes=V_nodes,
                     list_of_agents=list_of_agents,
                 )
-                logger.debug(f"✅ Constraint {const} set successfully...")
+                logger.info(f"✅ Constraint {const} set successfully...")
             except Exception as e:
                 logger.exception(f"❌ Error setting constraint {const} for cluster: {e}")
                 raise ValueError(f"Error setting constraint {const} for cluster")
@@ -659,7 +680,16 @@ class Builder(MVMTSPConfig):
         return interpolated_paths
     
 
-    def synchronize_agent_paths(self, paths:Dict ,cluster:Any, depot_id:int)->Dict: 
+    def synchronize_agent_paths(self, paths:Dict ,cluster:Any)->Dict: 
+       
+        # Calculate recharge steps 
+        problem_results = self.problem_results[f'Cluster_{cluster.id}']['agent_results']
+        tmp_times = defaultdict(float)
+        for agent in paths: 
+            tmp_times[agent] = calculate_recharge_steps(self.max_battery, energy_spent=problem_results[agent]['energy'])
+
+        
+       
         tmp_time_frame = 1000000000
         tmp_agent = None
         for agent, path in paths.items():
@@ -668,13 +698,14 @@ class Builder(MVMTSPConfig):
                 tmp_agent = agent
         logger.info(f"The shortest path length is {tmp_time_frame} for agent {tmp_agent}")
         cluster.timeframe = list(range(0,tmp_time_frame))
+        
         for agent, path in paths.items(): 
             for t in range(self.recharge_time_window): 
                 if len(path) < cluster.timeframe[-1]: 
                     time_diff = cluster.timeframe[-1] - len(path) +1
-                    idle = [(int(depot_id),int(depot_id),path[-1][2]+step) for step in range(time_diff)] 
+                    idle = [(int(cluster.depot_id),int(cluster.depot_id),path[-1][2]+step) for step in range(time_diff)] 
                     path.extend(idle)
-                path.append((int(depot_id),int(depot_id),cluster.timeframe[-1]+t))
+                path.append((int(cluster.depot_id),int(cluster.depot_id),cluster.timeframe[-1]+t))
 
         return paths 
     

dummy_app/models/topsis.py
@@ -17,19 +17,10 @@ class TOPSISPriority:
         self.weights = None 
 
 
-    def calculate_cluster_connectivity(self, cluster:pd.DataFrame, distance_metric:str)->Dict[str,float]: 
+    def calculate_cluster_connectivity(self, cluster:pd.DataFrame, distance_matrix)->Dict[str,float]: 
 
-        coords = cluster[['X_coords','Y_coords']].values 
-        num_nodes = coords.shape[0]
-        dist_matrix = None
-        
-        if distance_metric == 'geodesic':
-            tuple_coords = tuple(map(tuple, coords))  # Convert to tuple of tuples for geodesic
-            dist_matrix = squareform(pdist(tuple_coords, lambda u, v: geodesic(u, v).km))
-        
-        elif distance_metric == 'euclidean':
-            dist_matrix = squareform(pdist(coords,metric='euclidean'))
-
+        num_nodes = cluster.shape[0]
+        dist_matrix = distance_matrix
         # Average Distance between nodes 
         avg_distance = np.sum(dist_matrix) / (num_nodes * (num_nodes-1)) / 1000 
 
@@ -60,7 +51,7 @@ class TOPSISPriority:
         }
     
     
-    def gather_criteria(self, cluster:pd.DataFrame, cue_groups:Dict[int,List[object]], distance_metric:str) -> Dict[str,float]: 
+    def gather_criteria(self, cluster:pd.DataFrame, cue_groups:Dict[int,List[object]], distance_matrix:str) -> Dict[str,float]: 
 
         # find the users inside the areas of the cluster. 
         # the areas outside the cluster are not considered. 
@@ -69,7 +60,7 @@ class TOPSISPriority:
             if area_id in cue_groups.keys(): 
                num_customers += len(cue_groups[area_id])
 
-        conn_metrics = self.calculate_cluster_connectivity(cluster, distance_metric)
+        conn_metrics = self.calculate_cluster_connectivity(cluster, distance_matrix=distance_matrix)
         return {
             'Customers': num_customers,
             **conn_metrics

dummy_app/tools/common.py
@@ -1,14 +1,19 @@
 import gc
+import os 
 import sys 
+import joblib
 import pandas as pd 
 import numpy as np 
 import networkx as nx 
 import logging 
+from collections import defaultdict
+import matplotlib.pyplot as plt 
+
 
 from typing import Any, List, Dict, Union, Tuple
 from dummy_app.models.central_hubs import CentralHub
 from dummy_app.tools.graphs import is_eulerian_digraph
-
+from dummy_app.models.energy_model import DroneEnergyModel
 
 def deallocate_memory(variable:Any)->None:
     del variable 
@@ -37,9 +42,10 @@ def process_extraction(problem_builder:Any, extraction:Dict[str,Union[List[str],
     # Initialize structures 
     initial_population = {} 
     nodes_dict = {i: int(node) for i, node in enumerate(area_ids)}
-
+    
     cost_bundle = {'distance':cost_d, 'energy':cost_e,'travel_time':cost_t}
 
+    nodes_for_graph = nodes_dict.copy() 
     graph = create_model_graph(
         cost=cost_bundle, 
         nodes=nodes_dict,
@@ -48,11 +54,13 @@ def process_extraction(problem_builder:Any, extraction:Dict[str,Union[List[str],
 
     try:
         is_eulerian_digraph(graph)
+        if not is_eulerian_digraph(graph): 
+            graph = nx.eulerian_circuit(graph)
     except:
         raise ValueError("Graph is not eulerian")
 
+
     hub = CentralHub()
-    
     bridge_nodes = hub.get_bridge_nodes(
         graph=graph, 
         cluster_nodes = list(nodes_dict.keys()), 
@@ -72,10 +80,10 @@ def process_extraction(problem_builder:Any, extraction:Dict[str,Union[List[str],
         else: 
             allowed_visits = 1 
         R_points.append(allowed_visits)
-
+    
     assert len(cost_d) == len(cost_e) == len(cost_t) == len(R_points), \
     "Mismatch between distance, energy, travel_time and R_points dictionary length"
-
+    
     if hasattr(problem_builder, 'enable_ga') and problem_builder.enable_ga: 
         for agent in employed_agents: 
             solution_path, solution_cost = problem_builder.call_genetic_algorithm(
@@ -140,10 +148,7 @@ def get_session_duration(paths):
 
 
 def calculate_totals_from_paths(
-    paths: Any,
-    distance_costs: np.ndarray,
-    energy_costs: np.ndarray,
-    time_costs: np.ndarray
+    results
 ) -> Tuple:
     """
     Aggregate total distance, energy, and time across all agents' paths.
@@ -151,12 +156,10 @@ def calculate_totals_from_paths(
     total_distance = 0.0
     total_energy = 0.0
     total_time = 0.0
-
-    for agent in paths:
-        for current_node, next_node, _ in paths[agent]:
-            total_distance += distance_costs[current_node, next_node]
-            total_energy += energy_costs[current_node, next_node]
-            total_time += time_costs[current_node, next_node]
+    for agent in results:
+        total_distance += results[agent]['distance']
+        total_energy += results[agent]['energy']
+        total_time += results[agent]['time']
 
     return total_distance, total_energy, total_time
 
@@ -166,17 +169,100 @@ def extract_per_agent_metrics(
     paths: Any,
     distance_costs: np.ndarray,
     energy_costs: np.ndarray,
-    time_costs: np.ndarray
-) -> List[Dict[str, float]]:
+    time_costs: np.ndarray,
+    coverage_energy:float
+) -> Dict[str,Dict[str, float]]:
     """
     Return individual distance, energy, and time for each agent's path.
     """
-    results = []
+    results = defaultdict(dict)
+    
     for agent in paths:
-        dist = sum(distance_costs[i, j] for i, j, _ in agent)
-        energy = sum(energy_costs[i, j] for i, j, _ in agent)
-        duration = sum(time_costs[i, j] for i, j, _ in agent)
-
-        results.append({'distance': dist, 'energy': energy, 'time': duration})
+        visited_nodes = []
+        dist = 0.0 
+        energy = 0.0 
+        duration = 0.0 
+        for i, j, _ in paths[agent]:
+            if (i,j) in visited_nodes:
+                duration += 1
+                continue 
+            if i == j : 
+                energy  += coverage_energy 
+                dist += 0 
+                duration += 1
+                visited_nodes.append((i,j))
+                continue
+            dist += distance_costs[i, j]
+            energy += energy_costs[i, j] 
+            duration += time_costs[i, j]
+            visited_nodes.append((i,j))
+        results[agent] = {'distance': dist, 'energy': energy, 'time': duration}
 
     return results
+
+
+def load_scalers(): 
+    scaler_path = f"{os.getcwd()}/assets/scalers"
+    scalers = defaultdict()
+
+    for scaler_name in os.listdir(scaler_path): 
+        if not scaler_name.endswith('.pkl'):continue
+        filepath = os.path.join(scaler_path, scaler_name)
+        scaler = joblib.load(filepath)
+        name = scaler_name.split('.')[0]
+        name = name.split('_')[1:] 
+        name = "_".join(name)
+        scalers[name] = scaler 
+
+    return scalers 
+
+
+def denormalize_cost(scalers:defaultdict, cost:dict):
+    for cost_metric in cost.keys(): 
+        if cost_metric == 'travel_time': 
+            cost_metric = cost_metric.split('_')[-1]
+        for scaler in scalers.keys(): 
+            if cost_metric in scaler: 
+                cost[cost_metric] = scalers[scaler].inverse_transform(cost[cost_metric])
+
+    return cost             
+
+def load_generated_data(): 
+    cost = defaultdict()
+    data_path = f"{os.getcwd()}/assets/data"
+    for metric in os.listdir(data_path): 
+        name = metric.split('.')[0]
+
+        df = pd.read_csv(f'{data_path}/{metric}')
+        cost[name] = df.values.astype(np.float32)
+    
+    return cost
+
+
+
+def calculate_recharge_steps(max_battery:float, energy_spent:float):  
+
+    energy_deficit = max_battery - energy_spent 
+    energy_model = DroneEnergyModel(max_battery=max_battery) 
+    time_steps = 0 
+    while energy_deficit < max_battery: 
+        recharge_energy = energy_model.recover_energy()
+        energy_deficit +=  recharge_energy
+        time_steps += energy_model.dt 
+
+    time_steps  /= 60 # back into mins discrete  
+    return time_steps
+
+
+def draw_circular_graph(G:nx.DiGraph): 
+    colors_top_10=['tab:orange','tab:blue','tab:green','lightsteelblue']
+    #Draw graph
+    pos= nx.circular_layout(G)
+    nx.draw(G,pos,with_labels=True)
+
+    #Setting up legend
+    labels=['Top 10 deg cent','Top 10 bet cent','Top 10 deg and bet cent','no top 10']
+    for i in range(len(labels)):
+        plt.scatter([],[],label=labels[i],color=colors_top_10[i])
+    plt.legend(loc='center')
+    plt.show()